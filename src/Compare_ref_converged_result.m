200b619b5bda5333ef11824c0d7f7fbc6db71a12
RayY721 <74128049+RayY721@users.noreply.github.com>
2023-05-24 17:35:31 +0200

    GA converged to a solution different from the reference with a better side lobes level, a new script for plotting and comparing the pattern is named as Compare_ref_converged

--- src/Compare_ref_converged_result.m
+++ src/Compare_ref_converged_result.m
@@ -1,4 +1,8 @@
-% Compare the result from 'GA_intensively_mutation' and the paper. 
+%% The GA converged to a solution with filling rate of 76%
+% This script compare the array factor found by the
+% script "GA_reprod_mate_mutation.m" and the reference paper. 
+
+%% 
 lambda = 1550e-9;
 d = 0.5*lambda;
 Res = 3000;
@@ -7,25 +11,23 @@ N = 200;
 elementPos = (-(N-1)*d/2:d:(N-1)*d/2)';
 ang = 0;        % target angle
 
-load wopt.mat;
+load wopt_converged.mat;
 
 %%
-wr = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 1 1 1 1 1 0 0 1 1 1 1 1 1 1 0 1 1 1 1 1 0 1 0 0 0 0 1 0 1 0 1 1 1 0 0 1 0 0 1 0 1 1 1 0 0 0 1 0 1 0 1 1 0 1]';
-
-wl = flipud(wr);
-w_reprod = [wl;wr];
-AF_reprod = AF(elementPos,-90,90,Res,k,ang,true,w_reprod);
+wrefl = flipud(wrefr);
+wref = [wrefl;wrefr];
+AF_ref = AF(elementPos,-90,90,Res,k,ang,true,wref);
 ylim([-40 0])
 title('The result from reference')
 figure
-findpeaks(AF_reprod,'NPeaks',2,'SortStr','descend')
+findpeaks(AF_ref,'NPeaks',2,'SortStr','descend')
 title('The max side lobe of reference result')
 
 woptl = flipud(woptr);
 wopt = [woptl;woptr];      
 AF_opt = AF(elementPos,-90,90,Res,k,ang,true,wopt);
 ylim([-40 0])
-title('The result from mutation only search')
+title('The result from mate and mutation')
 figure
 findpeaks(AF_opt,'NPeaks',2,'SortStr','descend')
 title('The max side lobe of result from mutation only search')
